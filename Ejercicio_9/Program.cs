659f2b7a27c3681ffbcdecd47d2a435683c7245f
Gemino <71484444@idexperujapon.edu.pe>
2025-06-20 17:09:35 -0500

    Agregar archivos de proyecto.

---
+++ Ejercicio_9/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_9
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1 ; i < 11; i++)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
