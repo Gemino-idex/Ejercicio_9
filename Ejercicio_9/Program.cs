f35d69a28c0b1af681e643cae6b36698e55f6a62
Gemino <71484444@idexperujapon.edu.pe>
2025-06-26 17:32:01 -0500

    finalizado

--- Ejercicio_9/Program.cs
+++ Ejercicio_9/Program.cs
@@ -10,8 +10,10 @@ namespace Ejercicio_9
     {
         static void Main(string[] args)
         {
+            //for se usa para repetir el codigo hasta un número definido de veces
             for (int i = 1 ; i < 11; i++)
             {
+                //es para escribir lo que se repitió
                 Console.WriteLine(i);
             }
         }
